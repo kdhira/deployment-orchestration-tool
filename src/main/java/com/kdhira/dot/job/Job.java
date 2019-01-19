c462a1868a12f4ba888f072f071e0a2241070739
Kevin Hira <kevin@hira.nz>
2019-01-19 18:47:18 +1300

    Move child job ability to JobManifest

--- src/main/java/com/kdhira/dot/job/Job.java
+++ src/main/java/com/kdhira/dot/job/Job.java
@@ -1,6 +1,5 @@
 package com.kdhira.dot.job;
 
-import java.util.List;
 import java.util.Map;
 
 import com.kdhira.dot.resource.Resource;
@@ -15,21 +14,13 @@ public interface Job extends Resource {
 
     void setJobDescription(String jobDescription);
 
-    List<Job> getSubJobs();
-
-    void setSubJobs(List<Job> subjobs);
-
-    boolean getParallelExecution();
-
-    void setParallelExecution(boolean parallelExecution);
-
     Job getParent();
 
     void setParent(Job parent);
 
     String getFQJI();
 
-    boolean execute(boolean parallelExecution);
+    boolean execute();
 
     void link(Map<String, Resource> sharedResources);
 
